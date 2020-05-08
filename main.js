aaac2b719c2259aceeef433dfb500d587183ed3d
Tomasz Koza <tomaszkoza.mail@gmail.com>
2020-05-08 18:27:14 +0200

    Base commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const name='Tomek';
+const age=30;
+
+console.log(name);
+console.log(age);
+console.log(`hi my name is ${name} and I have ${age} years old`);
+console.log('hi my name is',name,'and i have',age,'years old');
+
