9ff17042f7ec915179f3dec91b5efd8868d82fb3
Tomasz Koza <tomaszkoza.mail@gmail.com>
2020-05-14 23:36:02 +0200

    Added scripts to main.js and js.html. in js.html scripts change content of second paragraph

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const name = "Tomek";
 const age = 30;
-
+const favouriteNumber = 14;
 console.log(name);
 console.log(age);
 console.log(`hi my name is ${name} and I have ${age} years old`);
 console.log("hi my name is", name, "and i have", age, "years old");
+console.log(`this is my name ${name} I have ${age} years old and ${favouriteNumber} is my favourite number`);
+
